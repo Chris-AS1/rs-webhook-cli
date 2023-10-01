659c57390f29ee007477e09ee7732ea1a417def6
Chris-AS1 <8493773+Chris-AS1@users.noreply.github.com>
2023-10-01 12:05:33 +0200

    moved stuff to utils, added configs and builder

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,15 @@
-use anyhow::anyhow;
-use anyhow::Context;
-use clap::{ArgAction, Parser};
-use reqwest;
-use reqwest::blocking::Client;
-use serde::Deserialize;
-use serde::Serialize;
-use serde_json::Value;
-use std::{fs, path::PathBuf};
-use webhook_cli::error::Error;
-
-#[derive(Deserialize, Debug, Serialize)]
-struct WebHookTemplate {
-    url: DestURL,
-    data: ReqwPayload,
-}
-
-#[derive(Deserialize, Debug, Serialize)]
-struct DestURL(String);
-
-#[derive(Deserialize, Debug, Serialize)]
-struct ReqwPayload(Value);
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    /// Webhook to execute
-    webhook: Option<String>,
-
-    /// Lists all available webhooks
-    #[arg(short, long, action = ArgAction::SetTrue)]
-    list: bool,
-
-    /// Value to inject, will be treated in order
-    #[arg(short, action = ArgAction::Append, value_name="VALUE")]
-    inject: Option<Vec<String>>,
-}
-
-fn list_hooks() -> Result<(), Error> {
-    let paths: Vec<PathBuf> = fs::read_dir("./inventory/")
-        .map_err(|e| anyhow!(e))?
-        .filter_map(|f| f.ok())
-        .filter(|f| match f.path().extension() {
-            Some(ex) => ex == "json",
-            None => false,
-        })
-        .map(|f| f.path())
-        .collect();
-
-    for path in paths {
-        println!("{}", path.display());
-    }
-
-    Ok(())
-}
-
-fn build_request(w: WebHookTemplate) -> Result<(), Error> {
-    let client = Client::new();
-    let body = serde_json::to_string(&w.data);
-
-    let res = client
-        .post(reqwest::Url::parse(w.url.0.as_str()).context("url not valid")?)
-        .body(body.context("something went wrong")?.clone());
-
-    println!("{:?}", res);
-
-    res.send().context("request failed")?;
-    Ok(())
-}
+use clap::Parser;
+use webhook_cli::{
+    error::Error,
+    utils::{Cli, Configs, ConfigsBuilder},
+};
 
 fn main() -> Result<(), Error> {
     let args = Cli::parse();
-    if args.list == true {
-        if args.webhook.is_some() || args.inject.is_some() {
-            Err(Error::InvalidArgsError)?
-        }
-        match list_hooks() {
-            Ok(_) => true,
-            Err(e) => Err(e)?,
-        };
-    };
+    let configs: Configs = ConfigsBuilder::default().ssl_verify(false).build();
 
-    if let Some(wbh) = &args.webhook {
-        let mut wbh_filename = String::from(wbh);
-        if wbh.ends_with(".json") {
-            let a = wbh.strip_suffix(".json").unwrap();
-            wbh_filename = a.to_string();
-        }
-
-        let content = fs::read_to_string(format!("./inventory/{}.json", wbh_filename))
-            .map_err(|e| anyhow!(e))?;
-        let payload_data: WebHookTemplate =
-            serde_json::from_str(&content).map_err(|e| anyhow!(e))?;
-
-        build_request(payload_data).map_err(|e| anyhow!(e))?
-    }
-
-    if let Some(vec) = &args.inject {
-        println!("Injecting values: {:?}", vec);
+    if let Err(e) = args.run(configs) {
+        eprintln!("The program encountered an error: {}", e);
     }
 
     Ok(())
---
+++ src/utils.rs
@@ -0,0 +1,160 @@
+use crate::error::Error;
+use anyhow::anyhow;
+use anyhow::Context;
+use clap::{ArgAction, Parser};
+use reqwest;
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::Value;
+use std::{fs, path::PathBuf};
+
+#[derive(Debug)]
+pub struct Configs {
+    inventory_path: &'static str,
+    user_agent: &'static str,
+    ssl_verify: bool,
+}
+
+#[derive(Debug)]
+pub struct ConfigsBuilder {
+    inventory_path: &'static str,
+    user_agent: &'static str,
+    ssl_verify: bool,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct WebHookTemplate {
+    url: String,
+    data: Value,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct ReqwPayload(Value);
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Webhook to execute
+    pub webhook: Option<String>,
+
+    /// Lists all available webhooks
+    #[arg(short, long, action = ArgAction::SetTrue)]
+    pub list: bool,
+
+    /// Value to inject, will be treated in order
+    #[arg(short, action = ArgAction::Append, value_name="VALUE")]
+    pub inject: Option<Vec<String>>,
+}
+
+impl Cli {
+    pub fn run(&self, c: Configs) -> Result<(), Error> {
+        if self.list {
+            if self.webhook.is_some() || self.inject.is_some() {
+                Err(Error::InvalidArgsError)?
+            }
+        } else {
+            match self.list_hooks(&c) {
+                Ok(_) => true,
+                Err(e) => Err(e)?,
+            };
+        };
+
+        if let Some(webhook) = &self.webhook {
+            let mut filename = String::from(webhook);
+            if filename.ends_with(".json") {
+                filename = String::from(filename.strip_suffix(".json").unwrap());
+            }
+
+            let content = fs::read_to_string(format!("./inventory/{}.json", filename))
+                .map_err(|e| anyhow!(e))?;
+
+            let json_content: WebHookTemplate =
+                serde_json::from_str(&content).map_err(|e| anyhow!(e))?;
+
+            self.build_request(json_content, &c)
+                .map_err(|e| anyhow!(e))?
+        }
+
+        if let Some(vec) = &self.inject {
+            println!("Injecting values: {:?}", vec);
+        }
+
+        Ok(())
+    }
+
+    fn list_hooks(&self, c: &Configs) -> Result<(), Error> {
+        let paths: Vec<PathBuf> = fs::read_dir(c.inventory_path)
+            .map_err(|e| anyhow!(e))?
+            .filter_map(|f| f.ok())
+            .filter(|f| match f.path().extension() {
+                Some(ex) => ex == "json",
+                None => false,
+            })
+            .map(|f| f.path())
+            .collect();
+
+        for path in paths {
+            println!("{}", path.display());
+        }
+
+        Ok(())
+    }
+
+    pub fn build_request(&self, w: WebHookTemplate, c: &Configs) -> Result<(), Error> {
+        let client = Client::builder()
+            .user_agent(c.user_agent)
+            .danger_accept_invalid_certs(!c.ssl_verify)
+            .build()
+            .unwrap();
+
+        let body = serde_json::to_string(&w.data).context("error while serializing")?;
+
+        let res = client
+            .post(reqwest::Url::parse(w.url.as_str()).context("couldn't parse URL")?)
+            .body(body);
+
+        println!("{:?}", res);
+
+        res.send().context("request failed")?;
+        Ok(())
+    }
+}
+
+impl Default for ConfigsBuilder {
+    fn default() -> Self {
+        Self {
+            inventory_path: "./inventory/",
+            user_agent: concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")),
+            ssl_verify: true,
+        }
+    }
+}
+
+impl ConfigsBuilder {
+    pub fn new() -> ConfigsBuilder {
+        ConfigsBuilder::default()
+    }
+
+    pub fn inventory_path(mut self, path: &'static str) -> ConfigsBuilder {
+        self.inventory_path = path;
+        self
+    }
+    pub fn user_agent(mut self, user_agent: &'static str) -> ConfigsBuilder {
+        self.user_agent = user_agent;
+        self
+    }
+
+    pub fn ssl_verify(mut self, ssl_verify: bool) -> ConfigsBuilder {
+        self.ssl_verify = ssl_verify;
+        self
+    }
+
+    pub fn build(self) -> Configs {
+        Configs {
+            inventory_path: self.inventory_path,
+            user_agent: self.user_agent,
+            ssl_verify: self.ssl_verify,
+        }
+    }
+}
